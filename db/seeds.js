7b879e7762d30cbee8bc644f38cdcc1ecb9fa0ec
Martin Allgood <martintallgood@gmail.com>
2018-07-10 14:23:42 +0100

    Adjusted seeds file and changes practice model to now be embedded in User model

--- db/seeds.js
+++ db/seeds.js
@@ -12,57 +12,120 @@ mongoose.connect(dbURI, (err, db) => {
       admin: true,
       password: 'martin',
       passwordConfirmation: 'martin',
-      practiceType: [{name: 'keiko'}, {name: 'kata'}]
+      sessions: [{
+        title: 'Practice for finals',
+        discipline: 'Keiko',
+        // date: 2018-07-10,
+        duration: 60,
+        note: 'Smashed it.'
+      }]
+      // practiceType: [{name: 'keiko'}, {name: 'kata'}]
     }, {
       username: 'othermartin',
       email: 'othermartin@othermartin.com',
       admin: true,
       password: 'othermartin',
       passwordConfirmation: 'othermartin',
-      practiceType: [{name: 'shiai'}, {name: 'kata'}]
+      sessions: [{
+        title: 'Practice for finals',
+        discipline: 'Keiko',
+        // date: 2018-07-10,
+        duration: 60,
+        note: 'Smashed it.'
+      }]
+      // practiceType: [{name: 'shiai'}, {name: 'kata'}]
     }, {
       username: 'linda',
       email: 'linda@linda.com',
       admin: true,
       password: 'linda',
       passwordConfirmation: 'linda',
-      practiceType: [{name: 'keiko'}, {name: 'kata'}]
+      sessions: [{
+        title: 'Practice for finals',
+        discipline: 'Keiko',
+        // date: 2018-07-10,
+        duration: 60,
+        note: 'Smashed it.'
+      }]
+      // practiceType: [{name: 'keiko'}, {name: 'kata'}]
     }, {
       username: 'mike',
       email: 'mike@mike.com',
       password: 'mike',
       passwordConfirmation: 'mike',
-      practiceType: [{name: 'keiko'}, {name: 'kata'}]
+      sessions: [{
+        title: 'Practice for finals',
+        discipline: 'Keiko',
+        // date: 2018-07-10,
+        duration: 60,
+        note: 'Smashed it.'
+      }]
+      // practiceType: [{name: 'keiko'}, {name: 'kata'}]
     }, {
       username: 'nic',
       email: 'nic@nic.com',
       password: 'nic',
       passwordConfirmation: 'nic',
-      practiceType: [{name: 'shiai'}, {name: 'kata'}]
+      sessions: [{
+        title: 'Practice for finals',
+        discipline: 'Keiko',
+        // date: 2018-07-10,
+        duration: 60,
+        note: 'Smashed it.'
+      }]
+      // practiceType: [{name: 'shiai'}, {name: 'kata'}]
     }, {
       username: 'stevan',
       email: 'stevan@stevan.com',
       password: 'stevan',
       passwordConfirmation: 'stevan',
-      practiceType: [{name: 'keiko'}, {name: 'kata'}]
+      sessions: [{
+        title: 'Practice for finals',
+        discipline: 'Keiko',
+        // date: 2018-07-10,
+        duration: 60,
+        note: 'Smashed it.'
+      }]
+      // practiceType: [{name: 'keiko'}, {name: 'kata'}]
     }, {
       username: 'rich',
       email: 'rich@rich.com',
       password: 'rich',
       passwordConfirmation: 'rich',
-      practiceType: [{name: 'keiko'}, {name: 'kata'}]
+      sessions: [{
+        title: 'Practice for finals',
+        discipline: 'Keiko',
+        // date: 2018-07-10,
+        duration: 60,
+        note: 'Smashed it.'
+      }]
+      // practiceType: [{name: 'keiko'}, {name: 'kata'}]
     }, {
       username: 'avon',
       email: 'avon@avon.com',
       password: 'avon',
       passwordConfirmation: 'avon',
-      practiceType: [{name: 'shiai'}]
+      sessions: [{
+        title: 'Practice for finals',
+        discipline: 'Keiko',
+        // date: 2018-07-10,
+        duration: 60,
+        note: 'Smashed it.'
+      }]
+      // practiceType: [{name: 'shiai'}]
     }, {
       username: 'bianca',
       email: 'bianca@bianca.com',
       password: 'bianca',
       passwordConfirmation: 'bianca',
-      practiceType: [{name: 'keiko'}]
+      sessions: [{
+        title: 'Practice for finals',
+        discipline: 'Keiko',
+        // date: 2018-07-10,
+        duration: 60,
+        note: 'Smashed it.'
+      }]
+      // practiceType: [{name: 'keiko'}]
     }]))
     .then(users => console.log(`${users.length} kendoka created`))
     .catch(err => console.log(err))
