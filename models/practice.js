21c6378561c6a93d5cec928a76d23df891239415
Linda Le <linchowmein@hotmail.com>
2018-07-10 11:31:06 +0100

    changed naming conventions

--- models/practice.js
+++ models/practice.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 
 const historySchema = new  mongoose.Schema({
-  timeLogged: String,
+  loggedTime: String,
   startTime: Number,
   notes: String
 });
 
-const trainingSchema = new mongoose.Schema({
-  trainingType: { type: String, required: 'Type of training is required'},
+const practiceSchema = new mongoose.Schema({
+  practiceType: { type: String, required: 'Type of practice is required'},
   description: String,
   venue: String,
   startTime: String,
   history: [ historySchema ]
 });
 
-module.exports = mongoose.model('Training', trainingSchema);
+module.exports = mongoose.model('Practice', practiceSchema);
