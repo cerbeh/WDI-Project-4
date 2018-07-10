b0e0fecfbdb4e7d9650f9ec1e2334414d8600d68
Linda Le <linchowmein@hotmail.com>
2018-07-10 18:31:03 +0100

    pushing for martin

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
+import Auth from '../../lib/Auth';
 
 class Navbar extends React.Component{
   state={
@@ -12,6 +13,10 @@ class Navbar extends React.Component{
     if(prevProps.location.pathname !== this.props.location.pathname){
       this.setState({ navbarOpen: false});
     }
+  }
+  handleLogout = () => {
+    Auth.logout();
+    this.props.history.push('/');
   }
   render(){
     return (
@@ -36,6 +41,8 @@ class Navbar extends React.Component{
         <div className={`navbar-menu${this.state.navbarOpen ? ' is-active' : ''}`}>
           <div className="navbar-end">
             <Link to="/users" className="navbar-item">All Users</Link>
+            {Auth.isAuthenticated() && <Link to={`/users/${currentUser._id}`}>My Profile</Link>}
+            {Auth.isAuthenticated() && <a onClick={this.handleLogout} className="navbar-item">Logout</a>}
           </div>
         </div>
       </nav>
