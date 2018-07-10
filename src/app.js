8f38f8cb0af9b2750fcc04870bdfc9e77326df8e
Linda Le <linchowmein@hotmail.com>
2018-07-10 15:14:37 +0100

    app.js and users index skeleton appear on page

--- src/app.js
+++ src/app.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import UsersIndex from './components/users/Index';
+import Home from './components/pages/Home';
 import 'bulma';
 import './scss/style.scss';
 class App extends React.Component {
   render() {
     return (
-      <main>
-        <section>
-          <div className="container">
-            <h1>TEAM CHARTREUSE</h1>
-            <Switch>
-              <Route path="/users" component={UsersIndex} />
-              {/* <Route exact path="/" component={About} /> */}
-            </Switch>
+      <BrowserRouter>
+        <main>
+          <section>
+            <div className="container">
+              <h1>TEAM CHARTREUSE</h1>
+              <Switch>
+                <Route path="/users" component={UsersIndex} />
+                <Route exact path="/" component={Home} />
+              </Switch>
 
-          </div>
-        </section>
-      </main>
+            </div>
+          </section>
+        </main>
+      </BrowserRouter>
     );
   }
 }
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const Home = () => {
-  return(
-    <section className="hero "
-  )
-}
+  return (
+    <section className="hero is-medium is-info">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">
+            kendo.io
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
