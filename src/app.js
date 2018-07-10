60d4cca70341d1ea0c2096d7eeadf2980fcd278a
Linda Le <linchowmein@hotmail.com>
2018-07-10 15:27:11 +0100

    navbar footer added

--- src/app.js
+++ src/app.js
@@ -20,6 +20,30 @@ class App extends React.Component {
 
             </div>
           </section>
+          <nav className="navbar is-link is-fixed-bottom" role="navigation">
+            <div className="navbar-brand">
+              <a className="navbar-item is-expanded  is-block has-text-centered">
+                <i className="fa fa-user"></i>
+                <p className="is-size-7">Account</p>
+              </a>
+              <a className="navbar-item is-expanded  is-block has-text-centered">
+                <i className="far fa-calendar-alt"></i>
+                <p className="is-size-7">Schedule</p>
+              </a>
+              <a className="navbar-item is-expanded is-block has-text-centered">
+                <i className="far fa-compass"></i>
+                <p className="is-size-7">Session</p>
+              </a>
+              <a className="navbar-item is-expanded  is-block has-text-centered">
+                <i className="fas fa-chart-line"></i>
+                <p className="is-size-7">Statistics</p>
+              </a>
+              <a className="navbar-item is-expanded  is-block has-text-centered">
+                <i className="fas fa-history"></i>
+                <p className="is-size-7">History</p>
+              </a>
+            </div>
+          </nav>
         </main>
       </BrowserRouter>
     );
