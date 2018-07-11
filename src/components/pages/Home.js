c645ff92eb411b800d4920845c28d1d04771ac38
Linda Le <linchowmein@hotmail.com>
2018-07-11 08:48:43 +0100

    added styling

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Redirect, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Auth from '../../lib/Auth';
 
 class Home extends React.Component {
@@ -25,20 +25,6 @@ class Home extends React.Component {
             {Auth.isAuthenticated() && <Link className="navbar-item" to={`/users/${Auth.getPayload().sub}`}><i className="fas fa-arrow-alt-circle-right fa-5x"></i></Link>}
 
           </div>
-        </div>
-        <div className="hero-foot">
-          <nav className="tabs is-boxed is-fullwidth">
-            <div className="container">
-              <ul>
-                <li className="is-active"><a>Overview</a></li>
-                <li><a>Modifiers</a></li>
-                <li><a>Grid</a></li>
-                <li><a>Elements</a></li>
-                <li><a>Components</a></li>
-                <li><a>Layout</a></li>
-              </ul>
-            </div>
-          </nav>
         </div>
       </section>
     </div>;
