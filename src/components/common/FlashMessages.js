7316581b33b504b5547fa889566bfdc22da235ee
Linda Le <linchowmein@hotmail.com>
2018-07-13 10:02:15 +0100

    flash message now times out

--- src/components/common/FlashMessages.js
+++ src/components/common/FlashMessages.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import Flash from '../../lib/Flash';
 
-const FlashMessages = () =>{
+class FlashMessages extends React.Component{
+  state ={
+    messages: null
+  }
   //stores messages for when we run the method getMessages through Flash
-  const messages = Flash.getMessages();
-  Flash.clearMessages();
-  return(
-    <div className="container">
-      {messages && Object.keys(messages).map(type => <div key={type} className={`notification is-${type}`}>{messages[type]}</div>
-      )}
-    </div>
-  );
-};
+  render(){
+    const messages = Flash.getMessages();
+    Flash.clearMessages();
+    setTimeout(() => this.setState({ messages: '' }), 2000);
+    return(
+      <div className="container is-flash">
+        {messages && Object.keys(messages).map(type => <div key={type} className={`notification is-${type}`}>{messages[type]}</div>
+        )}
+      </div>
+    );
+  }
+}
 
 export default FlashMessages;
