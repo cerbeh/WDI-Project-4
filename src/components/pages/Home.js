eb5f62c0c039d5a43b4dc34928849a5f919336aa
Linda Le <linchowmein@hotmail.com>
2018-07-18 12:51:41 +0100

    added animations to home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -11,26 +11,43 @@ class Home extends React.Component {
   render() {
     return <div>
       <section className="hero is-fullheight">
-        <div className="image-gallery">
-          <ul>
-            <li id="one">
-              <img src="https://i.imgur.com/TOExWVb.png" alt="Dipak Choudhury slaying Sam Murray"/>
-            </li>
-            <li id="two">
-              <img src="https://i.imgur.com/aBn2oq9.png" alt="GB team at Ijima cup"/>
-            </li>
-            <li id="three">
-              <img src="https://i.imgur.com/gVXOn0f.png" alt="Onigiri girls at London Cup"/>
-            </li>
-            <li id="four">
-              <img src="https://i.imgur.com/qbXJf7B.png" alt="Declan Tomás Clowry and Young Park in Seiza"/>
-            </li>
-          </ul>
+        <div className="split left slide-in-top">
+          <div className="centered">
+            <h1 className="title text-focus-in">KENDO</h1>
+          </div>
         </div>
+
+        <div className="split right slide-in-top-delayed">
+          <div className="centered">
+            <h1 className="title underline text-focus-in-delayed">KENDO.IO</h1>
+            <h2 className="subtitle text-focus-in-more-delayed">
+              Focus on being better than you were yesterday
+            </h2>
+            {!Auth.isAuthenticated() && <Link to="/login"><i className="fas fa-arrow-alt-circle-right fa-5x text-focus-in-more-delayed"></i></Link>}
+            {Auth.isAuthenticated() && <Link to={`/users/${Auth.getPayload().sub}`}><i className="fas fa-arrow-alt-circle-right fa-5x text-focus-in-more-delayed"></i></Link>}
+          </div>
+        </div>
+
+        {/* <div className="image-gallery">
+        <ul>
+        <li id="one">
+        <img src="https://i.imgur.com/TOExWVb.png" alt="Dipak Choudhury slaying Sam Murray"/>
+      </li>
+      <li id="two">
+      <img src="https://i.imgur.com/aBn2oq9.png" alt="GB team at Ijima cup"/>
+    </li>
+    <li id="three">
+    <img src="https://i.imgur.com/gVXOn0f.png" alt="Onigiri girls at London Cup"/>
+  </li>
+  <li id="four">
+  <img src="https://i.imgur.com/qbXJf7B.png" alt="Declan Tomás Clowry and Young Park in Seiza"/>
+</li>
+</ul>
+</div> */}
         <div className="hero-body">
           <div className="container home-container has-text-left">
             <div className="rectangle"></div>
-            <h1 className="title tracking-in-expand ">
+            <h1 className="title tracking-in-expand">
               kendo<em>.io</em>
             </h1>
             <h2 className="subtitle">
